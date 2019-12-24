046ec00666f1fa15358395c66474b2a0f6b4e821
Евгения Гладыщева <gladyscheva@gmail.com>
2019-12-24 16:52:24 +0200

    3

---
+++ WpfApp1/Task3/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var taskManager = new List<TaskManager>
+            {
+                new TaskManager {TaskName = "Grocery", Description = "Pick up grocery", Priority = 2},
+                new TaskManager {TaskName = "Laundry", Description = "Do my laundry", Priority = 1},
+                new TaskManager {TaskName = "Email", Description = "Check mail and reply on urgent", Priority = 3}
+            };
+            this.DataContext = taskManager;
+        }
+    }
+}
---
+++ WpfApp1/Task3/TaskManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class TaskManager
+    {
+        public string TaskName { get; set; }
+
+        public string Description { get; set; }
+
+        public int Priority { get; set; }
+    }
+}
