fe3182a6ecbbdaa71647c1fafdd6a8a3f37f1ddf
Евгения Гладыщева <gladyscheva@gmail.com>
2020-01-17 09:58:30 +0200

    1

---
+++ WpfApp5/WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Toy> toys;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            toys = new ObservableCollection<Toy>();
+            this.gridToys.ItemsSource = toys;
+        }
+
+        private void OnAddClick(object sender, RoutedEventArgs e)
+        {
+            this.toys.Add(new Toy { Id = Int32.Parse(this.toyId.Text), Item = this.toyItem.Text, Price = Decimal.Parse(this.toyPrice.Text), Quantity = Int32.Parse(this.toyQuantity.Text) });
+        }
+
+        private void OnRemoveClick(object sender, RoutedEventArgs e)
+        {
+            this.toys.RemoveAt(this.gridToys.SelectedIndex);
+        }
+
+        private void OnClearClick(object sender, RoutedEventArgs e)
+        {
+            this.toys.Clear();
+        }
+    }
+}
---
+++ WpfApp5/WpfApp5/Toy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp5
+{
+    public class Toy
+    {
+        public int Id { get; set; }
+
+        public string Item { get; set; }
+        
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
