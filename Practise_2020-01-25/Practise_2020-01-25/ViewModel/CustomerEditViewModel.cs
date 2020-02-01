d5c5e1c105e7172b50d04f3914240470937438e1
Evgie <tubarao@ukr.net>
2020-02-01 11:11:36 +0200

    first

---
+++ Practise_2020-01-25/Practise_2020-01-25/ViewModel/CustomerEditViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Practise_2020_01_25.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise_2020_01_25.ViewModel
+{
+    public class CustomerEditViewModel
+    {
+        public ObservableCollection<Customer> Customers { get; set; }
+
+        public Customer SelectedCustomer { get; set; }
+
+        public CustomerEditViewModel()
+        {
+            this.Customers = new ObservableCollection<Customer>()
+            {
+                new Customer{FirstName = "Tetyana", LastName = "Ryzhik", City = "Kharkiv"},
+                new Customer{FirstName = "Andrew", LastName = "Fedorchenko", City = "Kharkiv"},
+                new Customer{FirstName = "Vlad", LastName = "Radchenko", City = "Kharkiv"},
+            };
+        }
+    }
+}
