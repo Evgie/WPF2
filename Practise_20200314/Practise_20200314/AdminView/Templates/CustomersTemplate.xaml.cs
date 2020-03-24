e6efacc51bb986b62f5b5d31e6fba1726a2a5aed
Евгения Гладыщева <gladyscheva@gmail.com>
2020-03-24 11:22:53 +0200

    4

---
+++ Practise_20200314/Practise_20200314/AdminView/Templates/CustomersTemplate.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace View.AdminView.Templates
+{
+    /// <summary>
+    /// Interaction logic for CustomersTemplate.xaml
+    /// </summary>
+    public partial class CustomersTemplate : UserControl
+    {
+        public CustomersTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
